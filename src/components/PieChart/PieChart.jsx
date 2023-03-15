76e107e4828d0d0ebbccc9f16f7388a187f8bf16
Ollie Cross <iio.olivercross@gmail.com>
2023-03-15 22:30:18 +0000

    :zap: Filter by same cake day. No of large cakes refactored to account for 1 cake per 2 people shared

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -4,21 +4,41 @@ import { PieChart as Pie } from "react-minimal-pie-chart";
 import "./PieChart.scss";
 
 const PieChart = ({ cakeSizes }) => {
+  //Divide # large cakes by 2 as assuming 1 will be shared for a joint birthday, rounded up if more than 2
   const data = [
-    { title: "Big", value: cakeSizes[0], color: "#db2885" },
+    { title: "Big", value: Math.ceil(cakeSizes[0] / 2), color: "#db2885" },
     { title: "Small", value: cakeSizes[1], color: "#FFF" },
   ];
 
+  //If no cake data, render message
+  if (cakeSizes[0] === 0 && cakeSizes[1] === 0) {
+    return (
+      <div className="piechart">
+        <p className="piechart__noCake">Short on Cake :(</p>
+      </div>
+    );
+  }
+
   return (
-    <Pie
-      data={data}
-      animate
-      animationDuration={500}
-      animationEasing="ease-out"
-      center={[50, 11]}
-      radius={10}
-      viewBoxSize={[100, 22]}
-    />
+    <div className="piechart">
+      {cakeSizes[0] > 0 && (
+        <p className="piechart__largeCake">
+          Big Cakes ( {Math.ceil(cakeSizes[0] / 2)} )
+        </p>
+      )}
+      <Pie
+        data={data}
+        animate
+        animationDuration={500}
+        animationEasing="ease-out"
+        center={[45, 10]}
+        radius={8}
+        viewBoxSize={[90, 20]}
+      />
+      {cakeSizes[1] > 0 && (
+        <p className="piechart__smallCake">Small Cakes ( {cakeSizes[1]} )</p>
+      )}
+    </div>
   );
 };
 
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 //Styles
 import "./Table.scss";
 
 const Table = ({ employees }) => {
+  const [filtered, setFiltered] = useState([]);
+  const [activeFilter, setActiveFilter] = useState(false);
+
+  const filterEmployees = ({ innerHTML }) => {
+    const filteredByCakeDay = employees.filter(
+      (employee) => employee.cakeDay.slice(4, 15) === innerHTML
+    );
+    setFiltered(filteredByCakeDay);
+    setActiveFilter(true);
+  };
+
+  const resetFiltered = () => {
+    setFiltered(employees);
+    setActiveFilter(false);
+  };
+
+  useEffect(() => {
+    if (employees) {
+      setFiltered(employees);
+    }
+  }, [employees]);
   if (!employees) {
     return <p>Loading ...</p>;
   }
   return (
     <section className="table-container">
       <table className="table" data-testid="testTable">
+        {activeFilter && (
+          <button onClick={resetFiltered} className="table__reset">
+            X RESET
+          </button>
+        )}
         <thead className="table__head">
           <tr>
             <th>NAME</th>
@@ -17,14 +43,23 @@ const Table = ({ employees }) => {
             <th>CAKE SIZE</th>
           </tr>
         </thead>
-        {employees.length && (
+        {filtered.length && (
           <tbody className="table__body" data-testid="displayTbody">
-            {employees
+            {filtered
               .map((employee) => (
                 <tr className="table__row" key={employee.id}>
-                  <td>{employee.name}</td>
+                  <td>{employee.name.slice(0, 20)}</td>
                   <td>{new Date(employee.dob).toDateString().slice(4, 15)}</td>
-                  <td>{employee.cakeDay.slice(4, 15)}</td>
+                  {employee.cakeSize === "Large" ? (
+                    <td
+                      className="table__row--active"
+                      onClick={(e) => filterEmployees(e.target)}
+                    >
+                      {employee.cakeDay.slice(4, 15)}
+                    </td>
+                  ) : (
+                    <td>{employee.cakeDay.slice(4, 15)}</td>
+                  )}
                   <td>{employee.cakeSize}</td>
                 </tr>
               ))
@@ -32,7 +67,7 @@ const Table = ({ employees }) => {
           </tbody>
         )}
       </table>
-      {!employees.length && <p className="table__error">Add an employee!</p>}
+      {!filtered.length && <p className="table__error">Add an employee!</p>}
     </section>
   );
 };
